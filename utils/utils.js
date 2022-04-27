1a7cef193e00a8aeeab4dbc591cc4584ea4da479
John Kirch <john.kirch.business@gmail.com>
2022-04-27 12:42:44 -0400

    utils

---
+++ utils/utils.js
@@ -0,0 +1,8 @@
+const data = require('../data/web-minted.json');
+
+const countCavemen = async () => {
+  // const jsonFile = fetch('../data/minted.json')
+  data.find(caveman => caveman.)
+};
+
+countCavemen();
